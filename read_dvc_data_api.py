348dd18ca55f23fc7180d98119346a1bc2661886
Ashirwad Sharma <ashisharma@bebotechnologies.com>
2025-09-29 13:55:07 +0530

    fetch data using api

---
+++ read_dvc_data_api.py
@@ -0,0 +1,9 @@
+import dvc.api
+
+with dvc.api.open(
+    "data/data.xml",
+    repo="https://github.com/dgearset/dvc-basic.git",
+    rev="main"   # optional: specify branch, tag, or commit
+) as fd:
+    data = fd.read()
+    print(data)
